a849d6f45f222c498b3786223bec918adf7ede10
ansonqian19 <chengqian19@gmail.com>
2021-05-11 11:53:45 -0500

    update

--- netlify/functions/courses.js
+++ netlify/functions/courses.js
@@ -20,16 +20,16 @@
 //   BOTH the lecturer/course combination and the course as a whole.
 
 // === Domain model - fill in the blanks ===
-// There are 4 models: _____, _____, _____, _____
-// There is one many-to-many relationship: _____ <-> _____, which translates to two one-to-many relationships:
-// - One-to-many: _____ -> _____
-// - One-to-many: _____ -> _____
-// And one more one-to-many: _____ -> _____
+// There are 4 models: Courses_, _Lecturers__, Reviews__, _Sections____
+// There is one many-to-many relationship: __Courses___ <-> __Lecturers___, which translates to two one-to-many relationships:
+// - One-to-many: _Courses__ -> ____Section
+// - One-to-many: _Lecturers____ -> ___Section__
+// And one more one-to-many: _Reviews____ -> __Sections_
 // Therefore:
-// - The first model, _____, contains the following fields (not including ID): _____, _____ 
-// - The second model, _____, contains the following fields: _____
-// - The third model, _____, contains the following fields: _____, _____
-// - The fourth model, _____, contains the following fields, _____, _____, _____
+// - The first model, ___Courses__, contains the following fields (not including ID): _Course number____, _Course Name____ 
+// - The second model, __Lecturers___, contains the following fields: __Name___
+// - The third model, ___Sections__, contains the following fields: __CourseId___, ___LecturerId__
+// - The fourth model, __Reviews___, contains the following fields, _SectionId____, _Body___, __Rating__
 
 // allows us to use firebase
 let firebase = require(`./firebase`)
@@ -37,36 +37,59 @@ let firebase = require(`./firebase`)
 // /.netlify/functions/courses?courseNumber=KIEI-451
 exports.handler = async function(event) {
 
-  // get the course number being requested
 
+  // get the course number being requested
+  let courseNumber = event.queryStringParameters.courseNumber
+  
   // establish a connection to firebase in memory
+  let db = firebase.firestore()
 
   // ask Firebase for the course that corresponds to the course number, wait for the response
-
+  let courseQuery = await db.collection(`courses`).where(`number`,`==`,courseNumber).get()
+  
   // get the first document from the query
+  let courses = courseQuery.docs[0]
 
   // get the id from the document
-
+  let courseId = courses.id
+  
   // get the data from the document
+  let courseData = courses.data()
 
   // set a new Array as part of the return value
+  courseData.sections = []
 
   // ask Firebase for the sections corresponding to the Document ID of the course, wait for the response
+  let sectionQuery = await db.collection(`sections`).where(`courseId`,`==`,courseId).get()
 
   // get the documents from the query
+  let sections = sectionQuery.docs
 
   // loop through the documents
+  for (let i=0; i<sections.length;i++){
     // get the document ID of the section
-    // get the data from the section
-    // ask Firebase for the lecturer with the ID provided by the section; hint: read "Retrieve One Document (when you know the Document ID)" in the reference
-    // get the data from the returned document
-    // add the lecturer's name to the section's data
-    // add the section data to the courseData
-    // 🔥 your code for the reviews/ratings goes here
+    let sectionId = sections[i].id
 
+    // get the data from the section
+    let sectionData = sections[i].data()
+
+    // ask Firebase for the lecturer with the ID provided by the section; hint: read "Retrieve One Document (when you know the Document ID)" in the reference
+    let lecturerQuery = await db.collection(`lecturers`).doc(sectionData.lecturerId).get()
+
+    // get the data from the returned document
+    let lecturer = lecturerQuery.data()
+
+    // add the lecturer's name to the section's data
+    sectionData.lecturerName = lecturer.name
+
+    // add the section data to the courseData
+    courseData.sections.push(sectionData)
+
+    // 🔥 your code for the reviews/ratings goes here
+  }
   // return the standard response
   return {
     statusCode: 200,
-    body: `Hello from the back-end!`
+    body: JSON.stringify(courseData)
   }
 }
--- netlify/functions/posts.js
+++ netlify/functions/posts.js
@@ -21,29 +21,58 @@ let firebase = require(`./firebase`)
 // /.netlify/functions/posts
 exports.handler = async function(event) {
   // define an empty Array to hold the return value from our lambda
+  let returnValue = []
 
-  // establish a connection to firebase in memory
+  // establish a connection to firebase firestore in memory
+  let db = firebase.firestore()
 
   // perform a query against firestore for all posts, wait for it to return, store in memory
+  let postsQuery = await db.collection(`posts`).get()
 
   // retrieve the documents from the query
+  let posts=postsQuery.docs
+
 
   // loop through the post documents
+  for (let i=0;i<posts.length;i++){
     // get the id from the document
-    // get the data from the document
-    // create an Object to be added to the return value of our lambda
-    // get the comments for this post, wait for it to return, store in memory
-    // get the documents from the query
-    // loop through the comment documents
-      // get the id from the comment document
-      // get the data from the comment document
-      // create an Object to be added to the comments Array of the post
-      // add the Object to the post
-    // add the Object to the return value
+    let postId = posts[i].id
 
+    // get the data from the document
+    let postData = posts[i].data()
+    console.log(postData)
+    // create an Object to be added to the return value of our lambda
+    let postObject = {
+      id: postId,
+      imageUrl: postData.imgUrl,
+      numberOfLikes: postData.numOfLikes,
+      comments: []
+    }
+    // get the comments for this post, wait for it to return, store in memory
+    let commentsQuery = await db.collection(`comments`).where(`postId`,`==`,postId).get()
+    // get the documents from the query
+    let comments = commentsQuery.docs
+
+    // loop through the comment documents
+    for(let j=0; j<comments.length;j++){
+      // get the id from the comment document
+      let commentId = comments[j].id
+      // get the data from the comment document
+      let commentData = comments[j].data()
+      // create an Object to be added to the comments Array of the post
+      let commentObject = {
+        id: commentId,
+        body: commentData.body
+      }
+      // add the Object to the post
+      postObject.comments.push(commentObject)
+    } 
+    // add the Object to the return value
+    returnValue.push(postObject)
+  }
   // return value of our lambda
   return {
     statusCode: 200,
-    body: `Hello from the back-end!`
+    body: JSON.stringify(returnValue)
   }
 }
